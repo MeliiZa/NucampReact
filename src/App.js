8c4ab060a241af92434d028ae55e1b52d2764377
MeliiZa <melissazandman@icloud.com>
2020-05-28 13:53:43 -0700

    Add styling and card with react

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './components/DirectoryComponent';
 import './App.css';
-
+import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          campsites: CAMPSITES
+      };
+  }
+
     render() {
         return (
             <div className="App">
@@ -14,7 +21,7 @@ class App extends Component {
                     
                 </div>
                 </Navbar>
-                <Directory />
+                <Directory campsites={this.state.campsites} />
             </div>
         );
     }
