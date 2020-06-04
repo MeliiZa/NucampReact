77c94b07d8ee7a803f57ef16a571b66f576a6cce
MeliiZa <melissazandman@icloud.com>
2020-06-04 15:21:36 -0700

    Add route to home

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,34 +3,41 @@ import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import CampsiteInfo from "./CampsiteInfoComponent";
 
 class Main extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          campsites: CAMPSITES,
-          selectedCampsite: null
-      };
-  }
 
-  onCampsiteSelect(campsiteId) {
-    this.setState({selectedCampsite: campsiteId});
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
 
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        }
+
         return (
-            <div >
+            <div>
                 <Header />
-                <Directory campsites={this.state.campsites} onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)} />
-                < CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
             </div>
-
-            
         );
     }
 }
 
+            
 
   export default Main;
