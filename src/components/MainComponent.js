7d4491c443ba8e0acbd98dce08f6e1e16798271d
MeliiZa <melissazandman@icloud.com>
2020-06-04 16:34:40 -0700

    add contact us route

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import { CAMPSITES } from '../shared/campsites';
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import CampsiteInfo from "./CampsiteInfoComponent";
 
@@ -30,6 +31,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
