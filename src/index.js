6f35393ec90906469cd5244b227fa3f11c0b8b49
MeliiZa <melissazandman@icloud.com>
2020-06-04 14:06:17 -0700

    add header and footer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 import App from './App';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
