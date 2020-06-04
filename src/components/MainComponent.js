609d91903fcf23d562aa23a3b13fca00e29d0344
MeliiZa <melissazandman@icloud.com>
2020-06-04 16:45:23 -0700

    Add rendered cards

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,14 +6,19 @@ import Footer from "./FooterComponent";
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import CampsiteInfo from "./CampsiteInfoComponent";
-
+import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 class Main extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -21,7 +26,10 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home
+                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                partner={this.state.partners.filter(partner => partner.featured)[0]} />
             );
         }
 
